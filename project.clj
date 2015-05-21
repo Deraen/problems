20f013c6b2c414a05e977dca1ace5a3438d60dd6
Juho Teperi <juho.teperi@iki.fi>
2015-05-21 17:22:27 +0300

    :on-blur problem

--- project.clj
+++ project.clj
@@ -2,10 +2,30 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3126"]
-                 [reagent "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+                 [org.clojure/clojurescript "0.0-3269"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [reagent "0.5.0"]
+                 [figwheel "0.3.3"]]
+
+  :plugins [[lein-figwheel "0.3.3"]
+            [lein-cljsbuild "1.0.6"]]
 
   :source-paths ["src" "target/classes"]
+
+  :figwheel {:repl false
+             :http-server-root "public"
+             :server-port 3451}
+
+  :resource-paths ["out" "resources"]
+
+  :cljsbuild {:builds {:dev {:figwheel true
+                             :source-paths ["src"]
+                             :compiler {:main "hello-world.core"
+                                        :output-to "out/public/main.js"
+                                        :asset-path ""
+                                        :output-dir "out/public"
+                                        :optimizations :none
+                                        :pretty-print false}}}}
 
   :clean-targets ["out" "out-adv"])
--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,20 +1,45 @@
 (ns hello-world.core
-  (:require [reagent.core :as reagent]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as reagent]
+            [reagent.ratom :refer [atom] :refer-macros [reaction]]
+            [cljs.core.async :refer [timeout chan <! put!]]))
 
 (enable-console-print!)
 
-(defn throw-something []
-  (throw (js/Error. "foobar")))
+(defonce state (atom {:foo {:value "", :active? false}}))
 
-(defn ^:export catch-and-rethrow []
-  (try
-    (throw-something)
-    (catch js/Error e
-      (println "catch" e)
-      (throw e))))
+(defn click []
+  (go
+    (swap! state assoc-in [:foo :active?] true)
+    (<! (timeout 250))
+    (swap! state assoc-in [:foo :active?] false)))
 
-(defn error []
-  [:p (aget nil "foo")])
+(def actions (chan))
+
+(go
+  (loop []
+    (when (<! actions)
+      (js/console.log "event")
+      (click)
+      (recur))))
+
+(defn main []
+  (let [data (reagent/cursor state [:foo])]
+    (fn []
+      [:div
+       [:input
+        {:value (-> @data :value)
+         :on-change #(swap! data assoc-in [:value] (.. % -target -value))
+         :on-blur #(swap! data assoc-in [:not-pristine] true)
+         }]
+       [:button
+        {:type "button"
+         :on-click #(do (put! actions true) nil)}
+        "Click"]
+       (if (:active? @data)
+         "Active")])))
 
 (defn ^:export start []
-  (reagent/render-component [error] (js/document.getElementById "app")))
+  (reagent/render-component [main] (js/document.getElementById "app")))
+
+(start)
