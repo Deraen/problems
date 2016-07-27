89bfc2b23ab43c41f47b4e0a902aac1644264112
Juho Teperi <juho.teperi@iki.fi>
2016-07-28 01:47:30 +0300

    .

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+echo 1.9.89
+echo
+rm -r out
 lein trampoline run -m clojure.main build.clj
+
+echo
+echo latest
+echo
+rm -r out
+lein with-profile +latest trampoline run -m clojure.main build.clj
--- project.clj
+++ project.clj
@@ -2,33 +2,14 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3269"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [reagent "0.5.0"]
-                 [figwheel "0.3.3"]]
-
-  :plugins [[lein-figwheel "0.3.3"]
-            [lein-cljsbuild "1.0.6"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.89"]
+                 [org.clojure/core.async "0.2.385"]]
 
   :source-paths ["src" "target/classes"]
 
-  :figwheel {:repl false
-             :http-server-root "public"
-             :server-port 3451}
-
   :resource-paths ["out" "resources"]
 
-  :cljsbuild {:builds {:dev {:figwheel true
-                             :source-paths ["src"]
-                             :compiler {:main "hello-world.core"
-                                        :output-to "out/public/main.js"
-                                        :asset-path ""
-                                        :output-dir "out/public"}}
-                       :gh-pages {:source-paths ["src"]
-                                  :compiler {:main "hello-world.core"
-                                             :asset-path "/problems/reagent-strange-bug/out"
-                                             :output-to "gh-pages/reagent-strange-bug/main.js"
-                                             :output-dir "gh-pages/reagent-strange-bug/out"}}}}
+  :clean-targets ["out" "out-adv"]
 
-  :clean-targets ["out" "out-adv"])
+  :profiles {:latest {:dependencies [[org.clojure/clojurescript "1.9.146"]]}})
